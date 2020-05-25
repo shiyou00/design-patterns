3bb8bfde1715b11fa42cd62c2ad673c2cbe15dae
Frankshi <frankshi@easyops.cn>
2020-05-25 18:05:47 +0800

    feat(patterns): Aop

---
+++ decorator/dist/output.js
@@ -0,0 +1,10 @@
+var _class;
+
+let MyTestableClass = testable(_class = class MyTestableClass {
+}) || _class;
+
+function testable(target) {
+  target.isTestable = true;
+}
+
+console.log(MyTestableClass.isTestable);
